df907c9fdf2af58244c15f2ad5784c7706372544
Owen Hartin <owen.hartin@tocomail.com>
2015-10-14 21:19:22 -0500

    Adding simple win condition

--- game.rb
+++ game.rb
@@ -1,4 +1,8 @@
 puts("You see a creepy old house with a dirty cracked door.")
-input = gets()
-puts("You said: ")
-puts(input)
+input = gets().strip().downcase()
+
+if input == "open door"
+	puts("You win!")
+else
+	puts("Try again!")
+end
