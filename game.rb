61c383128ddf46318f6f1b23333612891057cedc
Owen Hartin <owen.hartin@tocomail.com>
2015-10-15 20:35:44 -0500

    Added path for unlocking the door

--- game.rb
+++ game.rb
@@ -1,8 +1,38 @@
-puts("You see a creepy old house with a dirty cracked door.")
-input = gets().strip().downcase()
+puts("You come upon an abandoned house with a dirty cracked door.")
+puts("The neglected sun-burnt lawn has a crooked mailbox as its only feature.")
 
-if input == "open door"
-	puts("You win!")
-else
-	puts("Try again!")
+won_game = false
+opened_mailbox = false
+took_key = false
+unlocked_door = false
+
+until won_game
+	input = gets().strip().downcase()
+	if input == "open the door"
+		if unlocked_door == true
+			won_game = true
+			puts("You win!")
+		else
+			puts("It is locked.")
+		end
+ 	elsif input == "open the mailbox"
+ 		opened_mailbox = true
+		puts("Inside is a shining silver key.")
+	elsif input == "get the key"
+		if opened_mailbox == true
+			took_key = true
+			puts("Taken.")
+		else
+			puts("What key?")
+		end
+	elsif input == "unlock the door"
+		if took_key == true
+			unlocked_door = true
+			puts "The rusty lock clicks."
+		else
+			puts "With what?"
+		end		
+	else
+		puts("Try again!")
+	end
 end
