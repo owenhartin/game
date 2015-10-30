ab560f4701ca3214d3ee678f2e16cb379a38d374
Brian Hartin <brian.hartin@banno.com>
2015-10-29 20:47:08 -0500

    Added basic health indicator and beginnings of first monster

--- game.rb
+++ game.rb
@@ -1,41 +1,68 @@
 require 'rubygems'
 require 'colorize'
 
-puts("You come upon an abandoned house with a dirty cracked door.")
-puts("The neglected sun-burnt lawn has a crooked mailbox as its only feature.")
+@empty_heart_picture = "\u2661"
+@full_heart_picture = "\u2665"
 
-won_game = false
-opened_mailbox = false
-took_key = false
-unlocked_door = false
+@hearts_remaining = 10
+@won_game = false
+@opened_mailbox = false
+@took_key = false
+@unlocked_door = false
 
-until won_game
-	input = gets().strip().downcase()
-	if input == "open the door"
-		if unlocked_door == true
-			won_game = true
-			puts("You win!".colorize(:yellow))
+def get_command_from_player1()
+  gets().strip().downcase()
+end
+
+def show_hearts()
+	puts(@full_heart_picture * @hearts_remaining)
+end
+
+def clear()
+	system("clear")
+end
+
+def show_screen(message)
+	clear()
+	show_hearts()
+	puts()
+	puts(message)
+	puts()
+	print(">")
+end
+
+show_screen("You come upon an abandoned house with a dirty cracked door.\nThe neglected sun-burnt lawn has a crooked mailbox as its only feature.")
+
+until @won_game
+	command = get_command_from_player1()
+
+	message = case command
+	when "open the door"
+		if @unlocked_door
+            "Inside, you see a dusty living room.  With a penguin.  Pecking on the wall."
 		else
-			puts("It is locked.")
+			"It is locked."
 		end
- 	elsif input == "open the mailbox"
- 		opened_mailbox = true
-		puts("Inside is a shining silver key.")
-	elsif input == "get the key"
-		if opened_mailbox == true
-			took_key = true
-			puts("Taken.")
+	when "open the mailbox"
+		@opened_mailbox = true
+		"Inside is a shining silver key."
+	when "get the key"
+		if @opened_mailbox
+			@took_key = true
+			"Taken."
 		else
-			puts("What key?")
+			"What key?"
 		end
-	elsif input == "unlock the door"
-		if took_key == true
-			unlocked_door = true
-			puts "Click."
+	when "unlock the door"
+		if @took_key
+			@unlocked_door = true
+			"Click."
 		else
-			puts "With what?"
-		end		
+			"With what?"
+		end
 	else
-		puts("Try again!")
+		"Try again!"
 	end
+
+	show_screen(message)
 end
